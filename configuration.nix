9e9f588f10bb623358e6502c78bc735884bc2cc3
SkeletonJelly47 <mikkelsson4@hotmail.com>
2026-07-09 23:27:28 +0300

    use nvf standalone template

--- configuration.nix
+++ configuration.nix
@@ -1,11 +1,31 @@
 {...}: {
+  # You may browse available options for nvf on the online manual. Please see
+  # <https://notashelf.github.io/nvf/options.html>
   vim = {
-    theme = {
+    theme.enable = true;
+
+    lsp = {
+      # Enable LSP functionality globally. This is required for modules found
+      # in `vim.languages` to enable relevant LSPs.
       enable = true;
-      name = "gruvbox";
-      style = "dark";
+
+      # You may define your own LSP configurations using `vim.lsp.servers` in
+      # nvf without ever needing lspconfig to do it. This will use the native
+      # API provided by Neovim > 0.11
+      servers = {};
     };
 
-    languages.nix.enable = true;
+    # Language support and automatic configuration of companion plugins.
+    # Note that enabling, e.g., languages.<lang>.diagnostics will automatically
+    # enable top-level options such as enableLSP or enableExtraDiagnostics as
+    # they are needed.
+    languages = {
+      enableFormat = true;
+      enableTreesitter = true;
+      enableExtraDiagnostics = true;
+
+      # Nix language and diagnostics.
+      nix.enable = true;
+    };
   };
 }
--- flake.nix
+++ flake.nix
@@ -1,17 +1,38 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOs/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nvf.url = "github:notashelf/nvf";
   };
 
-  outputs = {nixpkgs, ...} @ inputs: {
-    # TODO: perSystem via flake parts
-    packages.x86_64-linux = {
-      default =
-        (inputs.nvf.lib.neovimConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          modules = [./configuration.nix];
-        }).neovim;
-    };
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    # An abstraction over systems to easily provide the same package
+    # for multiple systems. This is preferable to abstraction libraries.
+    forEachSystem = nixpkgs.lib.genAttrs ["x86_64-linux"];
+  in {
+    packages = forEachSystem (system: let
+      pkgs = inputs.nixpkgs.legacyPackages.${system};
+
+      # Evaluate any and all modules to create the wrapped Neovim package.
+      neovimConfigured = inputs.nvf.lib.neovimConfiguration {
+        inherit pkgs;
+
+        modules = [
+          # Configuration module to be imported. You may define multiple modules
+          # or even import them from other files (e.g., ./modules/lsp.nix) to
+          # better modularize your configuration.
+          ./configuration.nix
+        ];
+      };
+    in {
+      # Packages to be exposed under packages.<system>. Those can accessed
+      # directly from package outputs in other flakes if this flake is added
+      # as an input. You may run those packages with 'nix run .#<package>'
+      default = self.packages.${system}.neovim;
+      neovimConfigured = neovimConfigured.neovim;
+    });
   };
 }
