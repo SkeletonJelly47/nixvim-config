796164d332d19ebb6e9d6e2ea26059d6f149d531
SkeletonJelly47 <mikkelsson4@hotmail.com>
2026-07-09 21:14:14 +0300

    fuck it we ball, nvf minimal

--- flake.nix
+++ flake.nix
@@ -1,44 +1,25 @@
 {
-  description = "A Nixvim configuration";
-
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
-    nixvim.url = "github:nix-community/nixvim";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOs/nixpkgs/nixos-unstable";
+    nvf.url = "github:notashelf/nvf";
   };
 
-  outputs =
-    { nixvim, flake-parts, ... }@inputs:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ];
-
-      perSystem =
-        { system, ... }:
-        let
-          configuration = nixvim.lib.evalNixvim {
-            # Specify the target system.
-            # Alternatively configure `nixpkgs` options in your modules.
-            inherit system;
-
-            # Import your Nixvim modules
-            modules = [ ./config ];
-
-            # You can use `extraSpecialArgs` to pass additional arguments to your module files
-            extraSpecialArgs = {
-              # inherit (inputs) foo;
-            };
-          };
-        in
-        {
-          # Run `nix flake check .` to verify that your config is not broken
-          checks.default = configuration.config.build.test;
-
-          # Lets you run `nix run .` to start nixvim
-          packages.default = configuration.config.build.package;
-        };
+  outputs = {nixpkgs, ...} @ inputs: {
+    # TODO: perSystem via flake parts
+    packages.x86_64-linux = {
+      default =
+        (inputs.nvf.lib.neovimConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          modules = [
+            {
+              config.vim = {
+                theme.enable = true;
+                treesitter.enable = true;
+              };
+            }
+          ];
+        })
+        .neovim;
     };
+  };
 }
--- readme.md
+++
@@ -1,5 +0,0 @@
-# TODO
-- [ ] try NVF instead
-- [ ] fix format
-- [ ] auto format on save
-- [ ] fix nixd
